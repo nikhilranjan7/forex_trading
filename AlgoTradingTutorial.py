1f7766bd1a40f1eb43b948909520123d34f3c065
Nikhil Ranjan <nikhilranjan7@gmail.com>
2017-10-28 00:24:40 +0530

    Add pattern finding

--- AlgoTradingTutorial.py
+++ AlgoTradingTutorial.py
@@ -1,11 +1,47 @@
+from __future__ import division
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import matplotlib.dates as mdates
 import numpy as np
+import time
+
+date, bid, ask = np.loadtxt('GBPUSD1d.txt', unpack = True, delimiter=',', converters={0:mdates.strpdate2num('%Y%m%d%H%M%S')})
+
+patternAr = []
+performanceAr = []
+
+def percentChange(startPoint, currentPoint):
+    return ((currentPoint-startPoint)/startPoint)*100
+
+def patternFinder():
+    avgLine = ((bid + ask)/2)
+    x = len(avgLine) - 30 # Last 30 datapoints remaining to be compared
+    y = 11
+    while y < x:
+        p1 = percentChange(avgLine[y-10], avgLine[y-9])
+        p2 = percentChange(avgLine[y-10], avgLine[y-8])
+        p3 = percentChange(avgLine[y-10], avgLine[y-7])
+        p4 = percentChange(avgLine[y-10], avgLine[y-6])
+        p5 = percentChange(avgLine[y-10], avgLine[y-5])
+        p6 = percentChange(avgLine[y-10], avgLine[y-4])
+        p7 = percentChange(avgLine[y-10], avgLine[y-3])
+        p8 = percentChange(avgLine[y-10], avgLine[y-2])
+        p9 = percentChange(avgLine[y-10], avgLine[y-1])
+        p10 = percentChange(avgLine[y-10], avgLine[y-0])
+
+        outcomeRange = avgLine[y+20:y+30]
+        currentPoint = avgLine[y]
+
+        print reduce(lambda x, y: x+y, outcomeRange) / len(outcomeRange)
+        print currentPoint
+        print '_____'
+        print p1, p2, p3, p4, p5, p6, p7, p8, p9, p10
+        y += 1
+        time.sleep(5555)
 
 def graphRawFX():
-    date, bid, ask = np.loadtxt('GBPUSD1d.txt', unpack = True, delimiter=',', converters={0:mdates.strpdate2num('%Y%m%d%H%M%S')})
+
     fig = plt.figure(figsize = (10, 7))
     ax1 = plt.subplot2grid((40, 40), (0, 0), rowspan = 40, colspan = 40)
 
@@ -19,10 +55,10 @@ def graphRawFX():
 
     ax1_2 = ax1.twinx()
     ax1_2.fill_between(date, 0, (ask - bid), facecolor = 'g', alpha = .3)
-    
+
     plt.subplots_adjust(bottom = .23)
 
     plt.grid(True)
     plt.show()
 
-graphRawFX()
+patternFinder()

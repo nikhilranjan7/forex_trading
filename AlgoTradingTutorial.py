ccddddc91c288c3cb193e1cea6bd57172c9e14fe
Nikhil Ranjan <nikhilranjan7@gmail.com>
2017-10-27 23:24:32 +0530

    better visualization

--- AlgoTradingTutorial.py
+++ AlgoTradingTutorial.py
@@ -11,12 +11,17 @@ def graphRawFX():
 
     ax1.plot(date, bid)
     ax1.plot(date, ask)
+    plt.gca().get_yaxis().get_major_formatter().set_useOffset(False)
 
     ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
     for label in ax1.xaxis.get_ticklabels():
         label.set_rotation(45)
-    plt.gca().get_yaxis().get_major_formatter().set_useOffset(False)
+
+    ax1_2 = ax1.twinx()
+    ax1_2.fill_between(date, 0, (ask - bid), facecolor = 'g', alpha = .3)
     
+    plt.subplots_adjust(bottom = .23)
+
     plt.grid(True)
     plt.show()
 
